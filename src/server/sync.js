db048ad6292594fdd02883b90ad7d02c5ce1b40c
nikaple <nikaple@qq.com>
2018-05-26 10:42:39 +0800

    add an additional `times` param to udp initialization

--- src/server/sync.js
+++ src/server/sync.js
@@ -11,127 +11,133 @@ const CMD = require('./cmd')
 const FIFTY_TICKS_PER_SECOND = 20
 
 class SyncServer {
-  constructor() {
-    this.server = dgram.createSocket('udp4')
-    this.tickRate = FIFTY_TICKS_PER_SECOND
-    this.tickMode = true
-    this.tickModeMap = {}
-    this.bindEvents()
-    this.startTicking()
-    return this.server
-  }
-
-  bindEvents() {
-    this.server.on('message', (msg, rinfo) => {
-      this.logReceived(msg, rinfo)
-      const buffer = ByteBuffer.wrap(msg, 'utf-8', ByteBuffer.LITTLE_ENDIAN)
-      const command = buffer.readByte()
-      switch (command) {
-        case CMD.INIT:
-          this.init(buffer, rinfo)
-          break
-        case CMD.SYNC:
-          this.sync(buffer, rinfo)
-          break
-      }
-    })
-  }
-
-  init(buffer, { address, port }) {
-    const userId = buffer.readCString()
-    const client = userManager.getClientByUserId(userId)
-    if (!client) return
-    client.set('udpIp', address)
-    client.set('udpPort', port)
-    debugLog(`Init udp connection... client address is ${address}:${port}`)
-    const msg = Buffer.alloc(1)
-    msg.writeInt8(CMD.UDP_SUCCESS)
-    this.directSend(msg, port, address)
-  }
-
-  sync(buffer, { address, port }) {
-    const groupId = buffer.readByte()
-    const groupIndex = buffer.readByte()
-    const room = buffer.readByte()
-    const currentGroup = gameManager.getGroupByAddress(address, port)
-    if (!currentGroup) {
-      return
-    }
-    const currentClient = currentGroup.clients[groupIndex - 1]
-    if (!currentClient) {
-      return
-    }
-    currentClient.set('currentRoom', room)
-    const message = buffer.slice(2).toBuffer()
-    this.broadcast({
-      group: currentGroup,
-      client: currentClient,
-      message,
-      filter: client => client.get('currentRoom') === room,
-    })
-  }
-
-  broadcast({ group, client, message, filter = client => true }) {
-    const groupIndex = client.get('groupIndex')
-    group.clients
-      .filter(filter)
-      .filter(currentClient => currentClient !== client)
-      .map(client => [client.get('udpIp'), client.get('udpPort')])
-      .forEach(([ip, udpPort]) => {
-        this.send(client, message, udpPort, ip)
-      })
-  }
-
-  tick() {
-    Object.keys(this.tickModeMap).forEach(key => {
-      const [ip, port] = key.split(':')
-      const msg = this.tickModeMap[key]
-      this.directSend(msg, parseInt(port), ip)
-      delete this.tickModeMap[key]
-    })
-  }
-
-  startTicking() {
-    if (!this.tickMode) {
-      throw new Error(
-        'Cannot begin ticking when not in tick mode. use setTickMode(true) first.'
-      )
-    } else if (this.tickInterval) {
-      throw new Error(
-        'Cannot start ticking when already ticking. Call stopTicking() first.'
-      )
+    constructor() {
+        this.server = dgram.createSocket('udp4')
+        this.tickRate = FIFTY_TICKS_PER_SECOND
+        this.tickMode = true
+        this.tickModeMap = {}
+        this.bindEvents()
+        this.startTicking()
+        return this.server
     }
 
-    this.tickInterval = setInterval(this.tick.bind(this), this.tickRate)
-  }
-
-  logReceived(msg, { address, port }) {
-    debugLog(
-      chalk.yellowBright(`Receiving from ${address}:${port}: %j`),
-      msg.toJSON().data
-    )
-  }
-
-  send(from, msg, port, ip) {
-    if (!this.tickMode) {
-      this.directSend(msg, port, ip)
-    } else {
-      const key = `${ip}:${port}`
-      this.tickModeMap[key] =
-        this.tickModeMap[key] === undefined
-          ? msg
-          : Buffer.concat([this.tickModeMap[key], msg])
+    bindEvents() {
+        this.server.on('message', (msg, rinfo) => {
+            this.logReceived(msg, rinfo)
+            const buffer = ByteBuffer.wrap(
+                msg,
+                'utf-8',
+                ByteBuffer.LITTLE_ENDIAN
+            )
+            const command = buffer.readByte()
+            switch (command) {
+                case CMD.INIT:
+                    this.init(buffer, rinfo)
+                    break
+                case CMD.SYNC:
+                    this.sync(buffer, rinfo)
+                    break
+            }
+        })
     }
-  }
 
-  directSend(msg, port, ip) {
-    this.server.send(msg, port, ip, () => {
-      debugLog(
-        chalk.greenBright(`Message sent to ${ip}:${port}! msg: %j`),
-        msg.toJSON().data
-      )
-    })
-  }
+    init(buffer, { address, port }) {
+        const initTimes = buffer.readByte()
+        const userId = buffer.readCString()
+        const client = userManager.getClientByUserId(userId)
+        if (!client) return
+        client.set('udpIp', address)
+        client.set('udpPort', port)
+        debugLog(`Init udp connection... client address is ${address}:${port}`)
+        const msg = Buffer.alloc(2)
+        msg.writeInt8(CMD.UDP_SUCCESS)
+        msg.writeInt8(initTimes, 1)
+        this.directSend(msg, port, address)
+    }
+
+    sync(buffer, { address, port }) {
+        const groupId = buffer.readByte()
+        const groupIndex = buffer.readByte()
+        const room = buffer.readByte()
+        const currentGroup = gameManager.getGroupByAddress(address, port)
+        if (!currentGroup) {
+            return
+        }
+        const currentClient = currentGroup.clients[groupIndex - 1]
+        if (!currentClient) {
+            return
+        }
+        currentClient.set('currentRoom', room)
+        const message = buffer.slice(2).toBuffer()
+        this.broadcast({
+            group: currentGroup,
+            client: currentClient,
+            message,
+            filter: client => client.get('currentRoom') === room,
+        })
+    }
+
+    broadcast({ group, client, message, filter = client => true }) {
+        const groupIndex = client.get('groupIndex')
+        group.clients
+            .filter(filter)
+            .filter(currentClient => currentClient !== client)
+            .map(client => [client.get('udpIp'), client.get('udpPort')])
+            .forEach(([ip, udpPort]) => {
+                this.send(client, message, udpPort, ip)
+            })
+    }
+
+    tick() {
+        Object.keys(this.tickModeMap).forEach(key => {
+            const [ip, port] = key.split(':')
+            const msg = this.tickModeMap[key]
+            this.directSend(msg, parseInt(port), ip)
+            delete this.tickModeMap[key]
+        })
+    }
+
+    startTicking() {
+        if (!this.tickMode) {
+            throw new Error(
+                'Cannot begin ticking when not in tick mode. use setTickMode(true) first.'
+            )
+        } else if (this.tickInterval) {
+            throw new Error(
+                'Cannot start ticking when already ticking. Call stopTicking() first.'
+            )
+        }
+
+        this.tickInterval = setInterval(this.tick.bind(this), this.tickRate)
+    }
+
+    logReceived(msg, { address, port }) {
+        debugLog(
+            chalk.yellowBright(`Receiving from ${address}:${port}: %j`),
+            msg.toJSON().data
+        )
+    }
+
+    send(from, msg, port, ip) {
+        if (!this.tickMode) {
+            this.directSend(msg, port, ip)
+        } else {
+            const key = `${ip}:${port}`
+            this.tickModeMap[key] =
+                this.tickModeMap[key] === undefined
+                    ? msg
+                    : Buffer.concat([this.tickModeMap[key], msg])
+        }
+    }
+
+    directSend(msg, port, ip) {
+        this.server.send(msg, port, ip, () => {
+            debugLog(
+                chalk.greenBright(`Message sent to ${ip}:${port}! msg: %j`),
+                msg.toJSON().data
+            )
+        })
+    }
 }
 
 module.exports = new SyncServer()
